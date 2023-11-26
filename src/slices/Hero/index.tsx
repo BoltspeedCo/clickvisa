dacb444b7e4be3d6b4f4cd63a1c1eda0d1e92bb2
mirulnasir <mirul@bestari.io>
2023-11-27 08:34:56 +1100

    added loop

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -32,7 +32,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       {isFilled.link(video) && video.url ? (
         <div className="w-full  h-[450px] lg:h-[680px] object-cover object-center relative overflow-hidden">
           <div className="absolute w-full  -top-[60px] h-[570px] lg:h-[800px]">
-            <DynamicVideoPlayer url={video.url} playing playsinline style={{ objectFit: 'cover' }} muted width={'100%'} height={'100%'} controls={false} />
+            <DynamicVideoPlayer url={video.url} playing playsinline style={{ objectFit: 'cover' }} loop muted width={'100%'} height={'100%'} controls={false} />
           </div>
         </div>
       ) : null}
