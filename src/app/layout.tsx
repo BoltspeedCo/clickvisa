84512aa619f176dfe1eef767559119bc3ba0939b
mirulnasir <mirul@bestari.io>
2024-02-16 12:55:57 +0800

    delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function Layout({
   return (
     <html lang="en">
       <body >
-        {children} <PrismicPreview repositoryName={repositoryName} />
+        {/* {children} <PrismicPreview repositoryName={repositoryName} /> */}
         {/* <div className="h-full w-full grid place-items-center"><span className="text-4xl">suspended.</span></div> */}
 
       </body>
